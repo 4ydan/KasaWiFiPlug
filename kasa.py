d60c9ea5f4d84928c4cd5cf6c43331458d99ff35
Aydan145 <53004139+Aydan145@users.noreply.github.com>
2020-04-05 18:36:44 +0200

    final

--- kasa.py
+++ kasa.py
@@ -1,9 +1,7 @@
 import socket
 import argparse
-from struct import pack
 
-# Predefined Smart Plug Commands
-# For a full list of commands, consult tplink_commands.txt
+# Predefined Commands
 command = {'info': '{"system":{"get_sysinfo":{}}}',
            'on': '{"system":{"set_relay_state":{"state":1}}}',
            'off': '{"system":{"set_relay_state":{"state":0}}}',
@@ -11,6 +9,22 @@ command = {'info': '{"system":{"get_sysinfo":{}}}',
            'reboot': '{"system":{"reboot":{"delay":1}}}',
            'reset': '{"system":{"reset":{"delay":1}}}'
            }
+
+parser = argparse.ArgumentParser(description="TP-Link Wi-Fi Smart Plug Client v")
+parser.add_argument("-t", "--target", metavar="<hostname>", required=True, help="Target hostname or IP address")
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("-c", "--command", metavar="<command>",
+                   help="Preset command to send. Choices are: " + ", ".join(command), choices=command)
+group.add_argument("-j", "--json", metavar="<JSON string>", help="Full JSON string of command to send")
+args = parser.parse_args()
+
+ip = args.target
+port = 9999
+
+if args.command is None:
+    command = args.json
+else:
+    command = command[args.command]
 
 
 def encrypt(string):
@@ -33,30 +47,13 @@ def decrypt(string):
     return result.decode('latin-1')
 
 
-parser = argparse.ArgumentParser(description="TP-Link Wi-Fi Smart Plug Client v")
-parser.add_argument("-t", "--target", metavar="<hostname>", required=True, help="Target hostname or IP address")
-group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument("-c", "--command", metavar="<command>",
-                   help="Preset command to send. Choices are: " + ", ".join(command), choices=command)
-group.add_argument("-j", "--json", metavar="<JSON string>", help="Full JSON string of command to send")
-args = parser.parse_args()
-
-ip = args.target
-port = 9999
-
-if args.command is None:
-    cmd = args.json
-
-else:
-    cmd = command[args.command]
-
 try:
-    sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock_tcp.connect((ip, port))
-    sock_tcp.send(encrypt(cmd))
-    data = sock_tcp.recv(2048)
-    sock_tcp.close()
-    print(decrypt(data[4:]))
+    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.connect((ip, port))
+    socket.send(encrypt(command))
+    receive = socket.recv(2048)
+    socket.close()
+    print(decrypt(receive[4:]))
 
 except socket.error:
-    quit("Cound not connect to host " + ip + ":" + str(port))
+    quit("Could not connect to " + ip + ":" + str(port))
